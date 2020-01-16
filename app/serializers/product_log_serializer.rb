82749af79e720a8b07abe153bfd6e02e34a9cdfe
Andy Read <andyreadpnw@gmail.com>
2020-01-16 11:01:27 -0800

    before refactor

--- app/serializers/product_log_serializer.rb
+++ app/serializers/product_log_serializer.rb
@@ -1,4 +1,4 @@
 class ProductLogSerializer < ActiveModel::Serializer
-  attributes :status, :priority, :issue_type, :issue_class, :site, :division, :environment, :log_body, :assigned, :cc, :date_due
+  attributes :id, :status, :priority, :issue_type, :issue_class, :site, :division, :environment, :log_body, :assigned, :cc, :date_due
   has_one :product
 end
